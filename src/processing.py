a715813a165a1b97e835304fa5cbdd607d9bf13b
екатерина ермакова <katka_z_jones@mail.ru>
2024-08-19 10:33:52 +0200

    add function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,17 @@
 from operator import itemgetter
+from typing import Dict, List
 
 
-def filter_by_state(operation_info_list: list, state="executed") -> list:
+def filter_by_state(operation_info_list: List[Dict], state: str = "executed") -> List:
     """Функция для фильтрации операций по состоянию"""
     filtered_list = []
     for operation in operation_info_list:
-        if operation.get("state").lower() == state:
+        if operation["state"].lower() == state:
             filtered_list.append(operation)
     return filtered_list
+
+
+def sort_by_date(operation_info_list: List[Dict], sort_reverse: bool = True) -> List:
+    """Функция сортировки операций по дате в порядке убывания"""
+    sorted_list = sorted(operation_info_list, key=itemgetter("date"), reverse=sort_reverse)
+    return sorted_list
