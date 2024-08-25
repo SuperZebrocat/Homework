527922d01828be57e235436a5068653746e8e275
екатерина ермакова <katka_z_jones@mail.ru>
2024-08-25 17:55:43 +0200

    masks.py and test_masks.py fixed

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,13 @@
-def get_mask_card_number(card_number: str) -> str:
+from typing import Any
+
+
+def get_mask_card_number(card_number: str) -> Any:
     """Функция возвращает маску номера карты"""
     if len(card_number) == 16:
         return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:16]}"
 
 
-def get_mask_account(account_number: str) -> str:
+def get_mask_account(account_number: str) -> Any:
     """Функция возвращает маску номера счета"""
     if len(account_number) == 20:
         return f"**{account_number[-4:]}"
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import pytest
 
 from src.masks import get_mask_account, get_mask_card_number
@@ -7,27 +9,19 @@ from src.masks import get_mask_account, get_mask_card_number
     "value, expected",
     [
         ("7000792289606361", "7000 79** **** 6361"),
-        ("hello", "Неверно введен номер карты"),
-        ("", "Неверно введен номер карты"),
+        ("", None),
+        ("700079228960", None),
     ],
 )
-def test_get_mask_card_number(value, expected):
+def test_get_mask_card_number(value: str, expected: Any) -> Any:
+    """Функция тестирования для функции get_mask_card_number"""
     assert get_mask_card_number(value) == expected
-
-
-def test_get_mask_card_number_not_str():
-    with pytest.raises(TypeError):
-        get_mask_card_number(123)
 
 
 @pytest.mark.parametrize(
     "value, expected",
-    [("73654108430135874305", "**4305"), ("hello", "Неверно введен номер счета"), ("", "Неверно введен номер счета")],
+    [("73654108430135874305", "**4305"), ("73654", None), ("", None)],
 )
-def test_get_mask_account(value, expected):
+def test_get_mask_account(value: str, expected: Any) -> Any:
+    """Функция тестирования для функции get_mask_account"""
     assert get_mask_account(value) == expected
-
-
-def test_get_mask_account_not_str():
-    with pytest.raises(TypeError):
-        get_mask_card_number(123)
