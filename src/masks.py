3b7f903737968f0b037b956c2710a93541dc81d9
екатерина ермакова <katka_z_jones@mail.ru>
2024-08-25 16:38:31 +0200

    masks.py fixed

--- src/masks.py
+++ src/masks.py
@@ -1,14 +1,10 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция возвращает маску номера карты"""
-    if len(card_number) == 16 and card_number.isdigit():
+    if len(card_number) == 16:
         return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:16]}"
-    else:
-        return "Неверно введен номер карты"
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция возвращает маску номера счета"""
-    if len(account_number) == 20 and account_number.isdigit():
+    if len(account_number) == 20:
         return f"**{account_number[-4:]}"
-    else:
-        return "Неверно введен номер счета"
