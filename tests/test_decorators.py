a131a6378a50b4d89714ff4a7f902c9841a085ca
екатерина ермакова <katka_z_jones@mail.ru>
2024-09-05 17:32:56 +0200

    changes in README.md fixed

---
+++ tests/test_decorators.py
@@ -0,0 +1,56 @@
+from typing import Any
+
+import pytest
+
+from src.decorators import log
+
+
+def test_log_exception(capsys: Any) -> None:
+    """Тест на вывод результата в консоль при возникновении исключения"""
+
+    @log(filename="")
+    def my_function(x, y):
+        return x / y
+
+    with pytest.raises(Exception):
+        my_function(1, 0)
+        captured = capsys.readouterr()
+        assert captured.out == "my_function error: division by zero. Inputs: (1, 0), {}\n"
+
+
+def test_log_func_is_ok(capsys: Any) -> None:
+    """Тест на вывод результата в консоль при корректной работе функции"""
+
+    @log(filename="")
+    def my_function(x, y):
+        return x / y
+
+    result = my_function(1, 1)
+    assert result == 1
+    captured = capsys.readouterr()
+    assert captured.out == "my_function ok\n\n"
+
+
+def test_log_in_file_exception(filename="mylog.txt") -> None:
+    """Тест на запись результата в файл при возникновении исключения"""
+
+    @log(filename)
+    def my_function(x, y):
+        return x / y
+
+    my_function(1, 0)
+    with open(filename, "a") as file:
+        file.write("my_function error: division by zero. Inputs: (1, 0), {}\n")
+
+
+def test_log_in_file_func_ok(filename="mylog.txt") -> None:
+    """Тест на запись результата в файл при корректной работе функции"""
+
+    @log(filename)
+    def my_function(x, y):
+        return x / y
+
+    result = my_function(1, 1)
+    assert result == 1
+    with open(filename, "a") as file:
+        file.write("my_function ok\n")
