60648267488025b11fea27d972e7edc1ace11292
екатерина ермакова <katka_z_jones@mail.ru>
2024-09-18 12:19:21 +0200

    Homework 12_2 done

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,33 @@
 import json
+import logging
 import os
-from typing import List
+from typing import Any, List
 
 PATH_FILE = os.path.abspath("../data/operations.json")
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+rel_file_path = os.path.join(current_dir, "../logs/utils.log")
+abs_file_path = os.path.abspath(rel_file_path)
 
-def get_operations_info(path_file: str) -> List:
+logger = logging.getLogger("utils")
+file_handler = logging.FileHandler(abs_file_path, mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
+def get_operations_info(path_file: str) -> Any:
     """Функция для получения данных о банковских операциях из JSON-файла"""
     try:
+        logger.info(f"Попытка получения данных из файла {path_file}")
         with open(path_file, "r", encoding="utf-8") as file:
             operations = json.load(file)
+            logger.info("Данные успешно получены")
             return operations
     except FileNotFoundError:
-        print(f'Ошибка: файл "{path_file}" не найден')
+        logger.error(f'Ошибка получения данных: файл "{path_file}" не найден')
         return []
     except json.JSONDecodeError:
-        print(f'Ошибка при чтении json-файла из файла "{path_file}"')
+        logger.error(f'Ошибка при чтении json-файла из файла "{path_file}"')
         return []
