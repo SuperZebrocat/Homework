b0d0b6b1f8ad492a1044b84cf80b55dd2c25adfa
екатерина ермакова <katka_z_jones@mail.ru>
2024-09-18 12:16:57 +0200

    masks.py fixed and logging added

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,41 @@
+import logging
 from typing import Any
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+rel_file_path = os.path.join(current_dir, "../logs/masks.log")
+abs_file_path = os.path.abspath(rel_file_path)
+
+
+logger = logging.getLogger("masks")
+file_handler = logging.FileHandler(abs_file_path, mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def get_mask_card_number(card_number: str) -> Any:
     """Функция возвращает маску номера карты"""
-    if len(card_number) == 16:
-        return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:16]}"
+    try:
+        if len(card_number) == 16:
+            logger.info("Маскируем номер карты пользователя")
+            return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:16]}"
+        else:
+            logger.error("Произошла ошибка: не верный номер карты")
+            raise ValueError()
+    except ValueError:
+        return None
 
 
 def get_mask_account(account_number: str) -> Any:
     """Функция возвращает маску номера счета"""
-    if len(account_number) == 20:
-        return f"**{account_number[-4:]}"
+    try:
+        if len(account_number) == 20:
+            logger.info("Маскируем номер счета пользователя")
+            return f"**{account_number[-4:]}"
+        else:
+            logger.error("Произошла ошибка: не верный номер счета")
+            raise ValueError()
+    except ValueError:
+        return None
