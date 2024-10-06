5973dea903e9cee341332419fc3d799a89f1010a
екатерина ермакова <katka_z_jones@mail.ru>
2024-10-06 23:02:35 +0200

    new fixtures for tests added

--- tests/conftest.py
+++ tests/conftest.py
@@ -84,3 +84,65 @@ def transactions_csv_xlsx() -> List:
         {"id": 4653427.0, "state": "PENDING", "currency_code": "CNY"},
         {"id": 4641894.0, "state": "EXECUTED", "currency_code": "SEK"},
     ]
+
+
+@pytest.fixture
+def operation_list_state() -> list:
+    return [
+        {"id": 1, "state": "EXECUTED"},
+        {"id": 2, "state": "CANCELED"},
+        {"id": 3, "state": "PENDING"},
+    ]
+
+
+@pytest.fixture
+def operation_list_sort_reverse() -> list:
+    return [
+        {"id": 1, "date": "2019-07-03T18:35:29.512364"},
+        {"id": 2, "date": "2018-06-30T02:08:58.425572"},
+        {"id": 3, "date": "2018-09-12T21:27:25.241689"},
+    ]
+
+
+@pytest.fixture
+def operation_list_currency_json() -> List:
+    return [
+        {"id": 1, "operationAmount": {"amount": "1000", "currency": {"name": "EUR", "code": "EUR"}}},
+        {"id": 2, "operationAmount": {"amount": "2000", "currency": {"name": "руб.", "code": "RUB"}}},
+        {"id": 3, "operationAmount": {"amount": "3000", "currency": {"name": "USD", "code": "USD"}}},
+        {"id": 4, "operationAmount": {"amount": "4000", "currency": {"name": "руб.", "code": "RUB"}}},
+    ]
+
+
+@pytest.fixture
+def operation_list_description() -> List:
+    return [
+        {"id": 1, "description": "Перевод организации"},
+        {"id": 2, "description": "Открытие вклада"},
+        {"id": 3, "description": "Перевод со счета на счет"},
+        {"id": 4, "description": "Перевод с карты на карту"},
+    ]
+
+
+@pytest.fixture
+def mock_operations() -> List:
+    return [
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {"amount": "1000.0", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160",
+        },
+        {
+            "id": 214024827,
+            "state": "EXECUTED",
+            "date": "2018-12-20T16:43:26.929246",
+            "operationAmount": {"amount": "2000.0", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации",
+            "from": "Счет 10848359769870775355",
+            "to": "Счет 21969751544412966366",
+        },
+    ]
